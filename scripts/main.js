63764dcba711fe777ecc3bfc090e5030c8c7dff0
Kochkina Ekaterina <gudrum1983@gmail.com>
2026-03-18 11:57:26 +0100

    Изменить количество прокрутов колеса с 2 до 3

--- scripts/main.js
+++ scripts/main.js
@@ -2,34 +2,49 @@
 // to show/hide modals
 /***********************************************/
 
-function hidemodal01(){
-    $('#modal01').removeClass('visible');
-    $('.sweet-overlay').css('display','none');
-    $('#spinBG').addClass('spinAround');
+function hidemodal01() {
+  $('#modal01').removeClass('visible');
+  $('.sweet-overlay').css('display', 'none');
+  $('#spinBG').addClass('spinAround');
+  setTimeout(function () {
+    $('#modal02').addClass('visible');
+    $('.sweet-overlay').css('display', 'block');
     setTimeout(function () {
-        $('#modal02').addClass('visible');
-        $('.sweet-overlay').css('display','block');
-        setTimeout(function () {
-          $('#success02').addClass('animate');
-          $('#successtip02').addClass('animateSuccessTip');
-          $('#successlong02').addClass('animateSuccessLong');
-        }, 500);
-    }, 7500);
-} 
+      $('#success02').addClass('animate');
+      $('#successtip02').addClass('animateSuccessTip');
+      $('#successlong02').addClass('animateSuccessLong');
+    }, 500);
+  }, 7500);
+}
 
-function hidemodal02(){
-    $('#modal02').removeClass('visible');
-    $('.sweet-overlay').css('display','none');
-    $('#spinBG').addClass('spinAround2');
+function hidemodal02() {
+  $('#modal02').removeClass('visible');
+  $('.sweet-overlay').css('display', 'none');
+  $('#spinBG').addClass('spinAround2');
+  setTimeout(function () {
+    $('#modal02_1').addClass('visible');
+    $('.sweet-overlay').css('display', 'block');
     setTimeout(function () {
-        $('#modal03').addClass('visible');
-        $('.sweet-overlay').css('display','block');
-        setTimeout(function () {
-          $('#success03').addClass('animate');
-          $('#successtip03').addClass('animateSuccessTip');
-          $('#successlong03').addClass('animateSuccessLong');
-        }, 500);
-    }, 7700);
+      $('#success02_1').addClass('animate');
+      $('#successtip02_1').addClass('animateSuccessTip');
+      $('#successlong02_1').addClass('animateSuccessLong');
+    }, 500);
+  }, 7700);
+}
+
+function hidemodal02_1() {
+  $('#modal02_1').removeClass('visible');
+  $('.sweet-overlay').css('display', 'none');
+  $('#spinBG').addClass('spinAround2_1');
+  setTimeout(function () {
+    $('#modal03').addClass('visible');
+    $('.sweet-overlay').css('display', 'block');
+    setTimeout(function () {
+      $('#success03').addClass('animate');
+      $('#successtip03').addClass('animateSuccessTip');
+      $('#successlong03').addClass('animateSuccessLong');
+    }, 500);
+  }, 7700);
 }
 
 
@@ -54,27 +69,28 @@ function getWeekDay(){
 // timer
 /***********************************************/
 
-function countdown(){
+function countdown() {
 
-    let mins = parseInt(document.getElementById("mins").innerHTML);
-    let secs = parseInt(document.getElementById("hsecs").innerHTML);
+  let mins = parseInt(document.getElementById("mins").innerHTML);
+  let secs = parseInt(document.getElementById("hsecs").innerHTML);
 
 
-    if(mins!=0 && secs==0){
-        nmins = mins -1; nsecs = 59;
-    }
-    else if(mins!=0 || secs!=0){
-        nmins = mins; nsecs = secs - 1;
-    }
-    else if(mins==0 && secs==0){
-        nmins = mins; nsecs = secs;
-    }
+  if (mins != 0 && secs == 0) {
+    nmins = mins - 1;
+    nsecs = 59;
+  } else if (mins != 0 || secs != 0) {
+    nmins = mins;
+    nsecs = secs - 1;
+  } else if (mins == 0 && secs == 0) {
+    nmins = mins;
+    nsecs = secs;
+  }
 
-    document.getElementById("mins").innerHTML = nmins;
-    document.getElementById("hsecs").innerHTML = nsecs;
+  document.getElementById("mins").innerHTML = nmins;
+  document.getElementById("hsecs").innerHTML = nsecs;
 
-    if(nsecs < 10) nsecs = "0" + nsecs;
-    document.getElementById("hsecs").innerHTML = nsecs;
+  if (nsecs < 10) nsecs = "0" + nsecs;
+  document.getElementById("hsecs").innerHTML = nsecs;
 }
 
 /***********************************************/
@@ -83,25 +99,30 @@ function countdown(){
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const body = document.querySelector('body');
-    const modalButtonFirst = document.getElementById('modalButtonFirst');
-    const modalButtonSecond = document.getElementById('modalButtonSecond');
-    setInterval('countdown()', 1000);
+  const body = document.querySelector('body');
+  const modalButtonFirst = document.getElementById('modalButtonFirst');
+  const modalButtonSecond = document.getElementById('modalButtonSecond');
+  const modalButtonSecond_1 = document.getElementById('modalButtonSecond_1');
+  setInterval('countdown()', 1000);
 
 
-    function clickListener(event) {
-      const target = event.target;
+  function clickListener(event) {
+    const target = event.target;
 
-      if (target === modalButtonFirst) {
-        hidemodal01();
-      }
-
-      if (target === modalButtonSecond) {
-        hidemodal02();
-      }
+    if (target === modalButtonFirst) {
+      hidemodal01();
     }
 
-    body.addEventListener('click', clickListener);
+    if (target === modalButtonSecond) {
+      hidemodal02();
+    }
+
+    if (target === modalButtonSecond_1) {
+      hidemodal02_1();
+    }
+  }
+
+  body.addEventListener('click', clickListener);
 })
 document.addEventListener("DOMContentLoaded", () => {
   const osTypeElement = document.getElementById("typeOS");
